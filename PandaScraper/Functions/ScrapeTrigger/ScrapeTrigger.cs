bee907588ed7f9e15437fd73e339da6b4872dac0
Jesse Carter <jesse.carter@diply.com>
2017-03-31 13:21:36 -0400

    Initial timer trigger test

---
+++ PandaScraper/Functions/ScrapeTrigger/ScrapeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace PandaScraper.Functions.ScrapeTrigger
+{
+    public class ScrapeTrigger
+    {
+        public static void Run(TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
